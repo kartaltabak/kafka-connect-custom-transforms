97b33551be6015df0126796093380ab2fefff401
Kartal Tabak <kartal.tabak@gmail.com>
2025-11-21 23:09:10 +0300

    CopyField smt is added.

--- src/main/kotlin/name/ekt/kafka/connect/transform/RenameFieldNamesRegEx.kt
+++ src/main/kotlin/name/ekt/kafka/connect/transform/RenameFieldNamesRegEx.kt
@@ -3,7 +3,6 @@ package name.ekt.kafka.connect.transform
 import org.apache.kafka.common.config.ConfigDef
 import org.apache.kafka.connect.connector.ConnectRecord
 import org.apache.kafka.connect.transforms.util.SimpleConfig
-import java.util.Locale.ENGLISH
 import java.util.regex.Pattern
 
 class RenameFieldNamesRegEx<R : ConnectRecord<R>>
@@ -15,7 +14,6 @@ class RenameFieldNamesRegEx<R : ConnectRecord<R>>
         }
     }
 
-    private lateinit var field: String
     private lateinit var replacement: String
     private lateinit var regexPattern: Pattern
     override fun configure(configs: Map<String, *>?) {
